ce7556f042110bf60e23980fd21941f1b0bc2ec0
Frankie Chow <freakchow@gmail.com>
2022-11-06 08:24:13 +0000

    Add my L1T20 to this frankie grallary.

---
+++ script/script.js
@@ -0,0 +1,481 @@
+/* 
+Here is the like icon's svg date.
+I can add it for object tag in html5,
+but I try use javascript to change it's css,
+so I add it in here.
+
+You can see my code after line 92.
+*/
+
+
+let likeme = `
+<svg id="like" xmlns="http://www.w3.org/2000/svg"
+	viewBox="2567 795 5489 5967">
+    <g>
+    <polygon points=" 5355,1755 5344,1715 5331,1677 5316,1639 5299,1603 5281,1567 5260,1531 5238,1497
+ 5214,1462 5189,1428 5162,1394 5134,1360 5105,1326 5075,1293 5044,1260 5012,1227
+ 4979,1194 4946,1162 4913,1131 4879,1100 4845,1069 4812,1040 4778,1012 4745,984
+ 4712,959 4680,934 4648,911 4617,890 4586,871 4556,854 4527,840 4498,828 4471,818
+ 4443,812 4417,808 4391,807 4365,810 4341,816 4317,824 4292,836 4268,850 4243,867
+ 4218,886 4193,908 4167,932 4141,958 4115,986 4088,1016 4061,1047 4033,1080 4005,1114
+ 3978,1150 3950,1186 3922,1224 3894,1262 3866,1301 3838,1340 3811,1380 3784,1420
+ 3758,1460 3733,1500 3708,1541 3685,1581 3662,1622 3641,1662 3620,1702 3602,1742
+ 3584,1782 3568,1823 3554,1863 3542,1903 3531,1944 3522,1985 3515,2027 3510,2070
+ 3507,2108 3506,2146 3506,2186 3508,2227 3512,2269 3518,2312 3525,2356 3535,2402
+ 3546,2448 3558,2496 3572,2545 3588,2595 3605,2646 3624,2698 3644,2751 3665,2805
+ 3686,2859 3709,2914 3732,2969 3755,3024 3779,3080 3803,3135 3827,3191 3851,3246
+ 3874,3300 3897,3354 3918,3408 3939,3460 3959,3512 3978,3562 3995,3611 4011,3659
+ 4025,3705 4038,3750 4048,3793 4057,3835 4063,3874 4068,3912 4070,3947 4070,3981
+ 4068,4012 4064,4042 4058,4069 4050,4095 4037,4123 4021,4149 4003,4171 3980,4190
+ 3955,4205 3927,4216 3895,4225 3861,4230 3824,4232 3784,4231 3742,4227 3698,4221
+ 3651,4213 3604,4203 3555,4191 3505,4179 3454,4166 3402,4152 3351,4138 3300,4125
+ 3249,4113 3199,4102 3150,4092 3103,4085 3057,4079 3013,4076 2971,4076 2931,4079
+ 2893,4085 2858,4095 2826,4108 2796,4125 2768,4146 2743,4171 2720,4199 2700,4230
+ 2685,4257 2672,4286 2660,4317 2649,4351 2639,4386 2629,4425 2621,4465 2614,4507
+ 2608,4552 2602,4599 2597,4648 2593,4699 2589,4752 2586,4806 2584,4862 2582,4920
+ 2581,4979 2580,5039 2579,5100 2579,5161 2579,5223 2580,5286 2580,5349 2581,5411
+ 2583,5474 2584,5536 2586,5598 2587,5659 2589,5719 2591,5778 2594,5836 2596,5892
+ 2598,5947 2601,6000 2604,6052 2607,6101 2610,6149 2614,6194 2617,6238 2621,6279
+ 2626,6318 2631,6354 2636,6389 2642,6421 2648,6452 2655,6480 2666,6515 2677,6547
+ 2689,6576 2700,6602 2712,6625 2723,6645 2733,6662 2743,6677 2752,6690 2760,6700
+ 2768,6709 2775,6716 2782,6722 2789,6726 2796,6729 2803,6732 2810,6733 2819,6734
+ 2829,6735 2841,6735 2854,6736 2870,6736 2889,6736 2911,6737 2936,6738 2965,6739
+ 2998,6740 3036,6742 3079,6744 3127,6745 3180,6747 3239,6749 3304,6750 3375,6750
+ 3415,6750 3457,6749 3501,6749 3547,6748 3595,6747 3646,6746 3698,6744 3752,6743
+ 3808,6741 3867,6739 3928,6737 3990,6734 4055,6732 4121,6729 4190,6726 4260,6724
+ 4332,6721 4406,6718 4481,6714 4558,6711 4637,6708 4716,6704 4797,6701 4879,6697
+ 4962,6694 5046,6690 5131,6686 5216,6682 5302,6678 5388,6674 5474,6670 5560,6666
+ 5647,6661 5733,6657 5819,6653 5904,6648 5989,6644 6073,6639 6157,6634 6239,6630
+ 6321,6625 6401,6620 6480,6615 6557,6610 6633,6605 6707,6599 6780,6594 6851,6589
+ 6920,6583 6987,6577 7052,6571 7115,6565 7175,6559 7234,6553 7290,6547 7344,6540
+ 7396,6533 7445,6526 7493,6519 7537,6512 7580,6504 7621,6496 7659,6488 7695,6480
+ 7754,6465 7807,6448 7854,6431 7895,6413 7931,6393 7961,6373 7985,6352 8005,6330
+ 8021,6307 8032,6283 8039,6258 8043,6233 8044,6207 8042,6180 8038,6153 8032,6126
+ 8024,6098 8015,6071 8005,6043 7994,6016 7983,5989 7972,5962 7961,5935 7951,5910
+ 7941,5884 7931,5860 7923,5837 7915,5814 7909,5792 7903,5771 7898,5752 7893,5733
+ 7889,5716 7885,5700 7880,5684 7875,5670 7865,5650 7852,5633 7836,5618 7816,5605
+ 7793,5594 7767,5584 7738,5577 7706,5570 7672,5565 7637,5561 7600,5558 7564,5555
+ 7527,5552 7491,5549 7456,5546 7424,5543 7394,5539 7367,5534 7344,5529 7324,5523
+ 7309,5516 7298,5508 7292,5499 7290,5490 7292,5480 7298,5471 7309,5461 7324,5452
+ 7343,5442 7365,5433 7391,5424 7420,5415 7451,5406 7485,5398 7519,5389 7555,5380
+ 7591,5371 7626,5362 7661,5352 7694,5341 7726,5330 7755,5318 7782,5305 7806,5291
+ 7828,5275 7846,5258 7862,5240 7875,5220 7885,5200 7894,5179 7902,5156 7909,5131
+ 7914,5105 7919,5077 7923,5047 7927,5016 7930,4984 7933,4951 7935,4917 7937,4882
+ 7938,4847 7939,4813 7939,4778 7939,4745 7938,4712 7936,4681 7933,4651 7929,4623
+ 7924,4597 7918,4573 7910,4552 7900,4532 7889,4515 7875,4500 7859,4487 7840,4477
+ 7819,4469 7794,4463 7766,4459 7735,4458 7702,4459 7665,4461 7627,4465 7586,4471
+ 7544,4477 7502,4484 7459,4492 7416,4499 7374,4506 7334,4513 7295,4519 7259,4524
+ 7226,4527 7197,4529 7171,4529 7150,4527 7133,4523 7121,4517 7113,4510 7110,4500
+ 7111,4490 7115,4480 7124,4468 7135,4455 7151,4442 7170,4427 7193,4412 7219,4396
+ 7248,4380 7279,4363 7313,4346 7348,4328 7385,4310 7423,4291 7462,4272 7500,4253
+ 7538,4234 7575,4214 7611,4195 7645,4175 7677,4155 7707,4134 7734,4114 7758,4093
+ 7778,4072 7795,4051 7809,4029 7819,4007 7826,3984 7830,3960 7831,3938 7830,3916
+ 7827,3893 7822,3868 7816,3843 7809,3816 7800,3787 7791,3758 7781,3727 7769,3695
+ 7757,3662 7745,3629 7732,3594 7718,3559 7703,3524 7688,3489 7672,3453 7656,3418
+ 7639,3383 7622,3349 7603,3316 7584,3284 7564,3254 7542,3225 7520,3198 7496,3173
+ 7471,3150 7445,3129 7417,3111 7387,3095 7355,3082 7321,3072 7284,3065 7245,3060
+ 7214,3058 7182,3058 7148,3060 7112,3064 7075,3069 7035,3076 6994,3086 6950,3097
+ 6905,3111 6858,3126 6809,3143 6758,3162 6706,3182 6652,3204 6597,3228 6541,3253
+ 6484,3278 6425,3305 6366,3333 6306,3361 6246,3390 6186,3419 6125,3448 6064,3477
+ 6004,3506 5945,3534 5885,3561 5827,3588 5770,3613 5714,3637 5659,3660 5606,3681
+ 5555,3700 5505,3717 5457,3732 5412,3745 5368,3756 5327,3764 5288,3770 5252,3773
+ 5218,3773 5186,3771 5157,3766 5131,3758 5107,3748 5085,3735 5065,3719 5047,3700
+ 5032,3678 5020,3653 5010,3626 5003,3595 4999,3562 4997,3527 4997,3488 5000,3448
+ 5005,3404 5012,3359 5022,3312 5033,3262 5045,3211 5059,3159 5075,3105 5091,3050
+ 5109,2993 5127,2936 5146,2879 5165,2821 5184,2763 5204,2705 5223,2647 5241,2589
+ 5259,2532 5276,2475 5293,2420 5308,2365 5322,2312 5335,2260 5346,2209 5355,2160
+ 5363,2112 5369,2066 5374,2022 5376,1979 5377,1938 5376,1898 5373,1860 5369,1824
+ 5363,1789" stroke-linejoin="bevel"/>
+    </g>
+</svg>
+`
+/*
+This is grallary website. all date item
+is a photo object.
+
+I define photo class at here.
+*/
+
+class photo {
+	constructor(index, title, link, width, height) {
+		// Maybe I will sort the grallary.
+		// so I manual add a index element.
+		this.index = index;
+		// this is photo's tile. it will display in webpage.
+		// User cannot change it.
+		this.title = title;
+		// It is map img tag's src to photo file folder and file name.
+		this.link = link;
+		// photo's width and height. It isn't very import date.
+		// can cancel it at future version.
+		this.width = width;
+		this.height = height;
+		// Mark it for save.
+		// If it's value is 1, the photo will display at saved page.
+		this.saveit = 0;
+		// Mark it is like.
+		// When user like this photo, javascript will set it to 1.
+		this.like = 0;
+		// Storage user's comment about the photo.
+		this.comment = "";
+	}
+}
+
+// init date in the webpage.
+let photo1 = new photo(1, "Taverl Bike", "images/grallary/bike_small.jpg", "400", "300");
+let photo2 = new photo(2, "Flower near the window", "images/grallary/flower_small.jpg", "532", "300");
+let photo3 = new photo(3, "Sheep on a hill", "images/grallary/sheep_small.jpg", "400", "300");
+let photo4 = new photo(4, "Central Library", "images/grallary/library_small.jpg", "400", "300");
+
+// The webpage is a grallary, so add the photos to grallary array.
+let grallary = [photo1, photo2, photo3, photo4];
+
+// This is the local db, program will copy the int photo date into localdb.
+// then the date will storage to localStorage.
+let localdb = [];
+
+// format photo item, howto display in webpage.
+// when photo display at Grallary and Saved mode.
+// It is very different.
+// If the photo display in [Saved] mode,
+// User cannot modify anything.
+// Just can brower his choice (saved) photo.
+// this is a function, program use this function,
+// he need to pass photo object to here.
+function pageformat(e, pageType) {
+	let photoShow;
+	let thislike = "";
+	let thissave = "";
+	switch (pageType) {
+		case "saved":
+
+			// Check Is user like this photo.
+			// If the user like this photo. 
+			// add a like icon beside the photo.
+			if (e.like == 1) {
+				thislike = likeme;
+			} 
+				
+			photoShow =`
+<photo id="${e.index}">
+	<img src="${e.link}" alt="${e.title}" 
+		width="${e.width}" height="${e.height}" />
+	<div id="${e.index}">
+       		<h1>${e.title}</h1>
+			<comment>
+        		${e.comment}
+			</comment>
+        	<like>${thislike}</like>
+	</div>
+</photo>
+`
+			break;
+
+		case "grallary":
+
+			// When webpage display in grallary mode.
+			// If the photo is user like, add the like icon.			
+			if ( e.like == 1 ) {
+				thislike = likeme;
+			}
+
+			// If the photo was user saved. add the "Saved"
+			// string beside the photo.
+			if ( e.saveit == 1 ) {
+				thissave = "Saved";
+			} 
+
+			photoShow = `
+<photo id="${e.index}">
+	<img src="${e.link}" alt="${e.title}" width="${e.width}" height="${e.height}" />
+	<div id="${e.index}">
+		<h1>${e.title}</h1>
+		<!-- user can write in here for commit the photo -->
+		<!-- when user write in here, he need to click the "comment" link. -->
+		<!-- then saved in localStorage. -->
+		<textarea id="${e.index}" rows="5" cols="33">
+${e.comment}
+		</textarea>
+
+		<!-- the user can use the three nav ( buttom ) to modify the photo's user data. -->
+		<nav id="comment" href="#">Comment</nav>
+		<nav id="savelater" href="#">Save for later</nav>
+		<nav id="like" href="#">Like</nav>
+
+		<!-- display user's data about the photo -->
+		<tag>
+			<like>${thislike}</like>
+			<saveit>${thissave}</saveit>
+		</tag>
+	</div>
+</photo>
+`
+			break;
+	}
+	return photoShow;
+}
+
+$(document).ready(function() {
+
+	// save localdb to client brower's localStorage.
+	// the data type is string. so let use the JSON to storage it.
+	function savelocaldb() {
+		window.localStorage.setItem("localdb", JSON.stringify(localdb));
+	}
+
+	// Get localStorage's localdb to assign to global value localdb. 
+	function getlocaldb() {
+		localdb = JSON.parse( window.localStorage.getItem("localdb") );
+	}
+
+	// Reset the photos display include Grallary aand Saved pages.
+	// finish this function. all event Handler will reset.
+	function resetPhoto() {
+		localdb = JSON.parse( window.localStorage.getItem("localdb") );
+		$("photolist").text("");
+		$("saveforlater").text("");
+		localdb.forEach((e) => {
+
+			$("photolist").append(
+				pageformat(e, "grallary")				
+			);
+
+			if ( e.saveit == 1 ) {
+				$("saveforlater").append( 
+					pageformat(e, "saved")
+				);
+			}
+		});
+	}
+
+	// Reset the Saved Page's photo display.
+	// Because the Saved Page just do display
+	// and user cannot modify anything.
+	// so don't need to reset the eventhandler.
+	function resetSavedPhoto() {
+		$("saveforlater").text("");
+		localdb.forEach((e) => {
+
+			if ( e.saveit == 1 ) {
+				$("saveforlater").append( 
+					pageformat(e, "saved")
+				);
+			}
+
+		});
+	}
+
+	// show pages , when program use this function
+	// to show the webpages. program need to
+	// tell the what page mode is program want to display.
+	function showWebPage(page) {
+		// resetPhoto();
+		switch (page) {
+			case "grallary":
+                $("saveforlater").hide();
+                $("photolist").show();
+				$("contactform").show();
+				$("contactform").children("div").children("textarea").html(
+					window.localStorage.getItem("contactstr")
+				)
+				$("contactdata").hide();
+				break;
+			case "saved":
+				$("photolist").hide();
+                $("saveforlater").show();
+				$("contactform").hide();
+				$("contactdata").show();
+				// Local the contact info in localStorage.
+				// But in the case, server cannot get user keyin message.
+				// Because javascript need ajax to send it.
+				$("contactdata").children("text").children("h2").html(
+					window.localStorage.getItem("contactstr")
+				)
+				break;
+		}
+	}
+
+	// When user click the <nav> tag, program will
+	// need to get the parent html element <div>'s id
+	// program will follow this id to modify photo in localdb.
+	function checkDivId(e) {
+		return parseInt(e.parent("div").attr("id")) ;
+	}
+
+	// When user every brower this webpage.
+	// program will check global localdb, if localdb is null.
+	// ( not set before )
+	// program use the global grallary ver to init it.
+	// when user delete the brower cashs.
+	// program will use the global grallary var to reinit it.
+	getlocaldb();
+
+	if (localdb === null) {
+		window.localStorage.setItem("localdb", JSON.stringify(grallary));
+	}
+
+	// load the header. in header, there is my logo, webpage name
+	// and a dropdown menu. because header and photo is in different <div>
+	// so If I refresh the photo display, will not clear the header's event handler.
+	$("header").load("includeHtml/header.html", function() {
+        $("myName").load("includeHtml/myName.html");
+        $("navbar").load("includeHtml/navigationBar.html", function() {
+			$("nav.dropdown").click(function() {
+				$(this).children("div").toggle("slow");
+			});                                                             
+			
+            $("a#grallary").on("click", function() {
+				showWebPage("grallary");
+            });
+
+            $("a#saved").on("click", function() {
+				// reset the Saved Page.
+				// because when user make every change in grallary.
+				// maybe will change the saved page's content.
+				// so when program every display saved page.
+				// first reset it.
+				resetSavedPhoto();
+				showWebPage("saved");
+            });
+			
+
+        });
+	});
+
+	// load contact from.
+	$("contact").load("includeHtml/contact.html", function() {
+		$("nav#contact").on("click", function() {
+			console.log("Click the contact nav.");
+			// Add to contact data to localStorage.
+			window.localStorage.setItem("contactstr", 
+				$(this).prev("textarea").val()
+			);
+		});
+	});
+
+	// load <lab> -- grallary page.
+	// under the tag will show or display all photo content.
+	$("lab").load("includeHtml/lab.html", function() {
+
+		showWebPage("grallary")
+
+		// first time load <lab> need resetPhoto.
+		// But Please remember active the eventhandler.
+		resetPhoto();
+
+		// Click comment <nav> button
+		$("nav#comment").on("click", function() {
+
+			console.log("click comment")
+
+			// check the div's id and find it in the localdb.
+			let divid = checkDivId($(this));
+			let a = $(this) ;
+	        
+			// before every time click the localdb.
+			// get the localdb in localStorage.
+			getlocaldb();
+			localdb.forEach((photo) => {
+				if ( photo.index == divid ) {
+					// asign "textarea" content to photo.comment.
+					photo.comment = a.prev("textarea").val();
+					// after assign the comment to photo.comment.
+					// storage the global var localdb to localStorage's localdb.
+					savelocaldb();
+				}
+			});
+
+		});
+
+		// Click like <nav> -- button.
+		$("nav#like").on("click", function() {
+
+			console.log("click like");
+
+			let divid = checkDivId($(this)) ;
+			let a = $(this) ;
+
+			getlocaldb();
+			localdb.forEach((photo) => {
+				if ( photo.index == divid ) {
+					photo.like = 1;
+					savelocaldb();
+
+					let like = a.next("tag").children("like");
+					// reset the <like></like> in user display message block.
+					like.text("");
+					// display likeme's icon.
+					like.append(likeme);
+
+					// add a animate to photo's image.
+					// when user click the like <nav> -- button
+					// will scale photo image bigger.
+					let orgWidth = a.parent("div").parent("photo").children("img").attr("width");
+					let orgHeight = a.parent("div").parent("photo").children("img").attr("height");
+					let imgWidth = orgWidth * 1.33 ;
+					let imgHeight = orgHeight * 1.33;
+
+					// Scale the photo and chained.
+					a.parent("div").parent("photo").children("img").animate(
+						{
+							width: imgWidth,
+							height: imgHeight,
+						}, 4000
+					).animate(
+						{
+							width: orgWidth,
+							height: orgHeight,
+						}, 4000
+					);
+				}
+			});
+
+		});
+
+
+		// check the savelater <nav> -- button.
+		$(`nav#savelater`).on("click", function() {  // here use a maybe have bug.
+
+			console.log("click save");
+
+			let divid = checkDivId($(this)) ;
+			let a = $(this) ;
+	        
+			getlocaldb();
+			localdb.forEach((photo) => {
+				if ( photo.index == divid ) {
+	
+					if ( photo.saveit != 1 ) {
+	
+						photo.saveit = 1;
+						savelocaldb();
+
+/* 						// ShowPage need design.
+						$("saveforlater").append( 
+							pageformat(e, "saved")
+							); */
+
+						// show "Saved" at user info message block.
+						a.next().next("tag").children("saveit").text("Saved");
+
+					};
+				};
+			});
+
+			let coutSave = 0
+
+			getlocaldb();
+			localdb.forEach((photo) => {
+				// cout how many photo is saved and use alert window to show this infomation.
+				if ( photo.saveit == 1 ) {
+					coutSave = coutSave + 1;
+				}
+			});
+			
+			alert(`You save ${coutSave} pages`);  
+		});	
+	});
+	
+        $("footer").load("includeHtml/footer.html");
+
+});
